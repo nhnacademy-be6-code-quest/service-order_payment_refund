e22a4d5a5310dd4c188bbd153e7f72eebf2c4ef2
Virtus_Chae <ybd4731@gmail.com>
2024-06-20 09:14:02 +0900

    feat: Controller -> RestController

--- src/main/java/com/nhnacademy/payment/controller/PaymentController.java
+++ src/main/java/com/nhnacademy/payment/controller/PaymentController.java
@@ -3,17 +3,17 @@ package com.nhnacademy.payment.controller;
 import com.nhnacademy.payment.domain.Payment;
 import com.nhnacademy.payment.service.PaymentService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * @author Virtus_Chae
  * @version 0.0
  */
-@Controller
+@RestController
 public class PaymentController {
     PaymentService paymentService;
 
--- src/main/java/com/nhnacademy/payment/controller/RefundController.java
+++ src/main/java/com/nhnacademy/payment/controller/RefundController.java
@@ -1,19 +1,19 @@
 package com.nhnacademy.payment.controller;
 
 import com.nhnacademy.payment.domain.Payment;
-import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 
 /**
  * @author Virtus_Chae
  * @version 0.0
  */
-@Controller
+@RestController
 public class RefundController {
     /**
      * 사용자에게 환불 창을 보여 줍니다. 미완성입니다.
