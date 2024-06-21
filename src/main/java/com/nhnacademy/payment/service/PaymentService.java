fae49713b01313c229850b6b72c77e65d99f0cab
Virtus_Chae <ybd4731@gmail.com>
2024-06-21 16:26:45 +0900

    Payment Entity 속성 수정에 따라 변경

--- src/main/java/com/nhnacademy/payment/service/PaymentService.java
+++ src/main/java/com/nhnacademy/payment/service/PaymentService.java
@@ -26,14 +26,15 @@ public class PaymentService {
     public void savePayment(PaymentRequestDto paymentRequestDto) {
         Order order = orderRepository.findById(paymentRequestDto.orderId()).orElse(null);
 
+
         // 여기에서 이상이 있는 듯 -> Payment Method 가 없어서 문제가 생긴 것으로 보임
         PaymentMethod paymentMethod = paymentMethodRepository.findById(paymentRequestDto.paymentMethodId()).orElse(null);
         Payment payment = Payment.builder()
             .order(order)
-            .payTime(LocalDateTime.now())
-            .clientDeliveryAddressId(paymentRequestDto.clientDeliveryAddressId())
             .paymentMethod(paymentMethod)
             .couponId(paymentRequestDto.couponId())
+            .payAmount(paymentRequestDto.payAmount())
+            .payTime(LocalDateTime.now())
             .build();
 
         paymentRepository.save(payment);
@@ -44,11 +45,11 @@ public class PaymentService {
 
         return PaymentResponseDto.builder()
             .paymentId(payment.getPaymentId())
-            .orderId(payment.getOrder().getOrderId())
-            .payTime(payment.getPayTime())
-            .clientDeliveryAddressId(payment.getClientDeliveryAddressId())
+            .order(payment.getOrder())
             .paymentMethod(payment.getPaymentMethod())
             .couponId(payment.getCouponId())
+            .payAmount(payment.getPayAmount())
+            .payTime(payment.getPayTime())
             .build();
     }
 }
