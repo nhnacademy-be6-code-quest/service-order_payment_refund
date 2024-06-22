8864836c2d4560d72ede35d85814b3c505c32808
Virtus_Chae <ybd4731@gmail.com>
2024-06-22 22:00:49 +0900

    interface 에서 접근 제어자 제거

--- src/main/java/com/nhnacademy/orderpaymentrefund/service/payment/PaymentService.java
+++ src/main/java/com/nhnacademy/orderpaymentrefund/service/payment/PaymentService.java
@@ -4,6 +4,6 @@ import com.nhnacademy.orderpaymentrefund.dto.payment.request.PaymentRequestDto;
 import com.nhnacademy.orderpaymentrefund.dto.payment.response.PaymentResponseDto;
 
 public interface PaymentService {
-    public void savePayment(PaymentRequestDto paymentRequestDto);
-    public PaymentResponseDto findPaymentByPaymentId(Long paymentId);
+    void savePayment(PaymentRequestDto paymentRequestDto);
+    PaymentResponseDto findPaymentByPaymentId(Long paymentId);
 }
