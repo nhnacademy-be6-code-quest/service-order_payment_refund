2c50bea5bbe544b73f602f6b36085a62719792de
Virtus_Chae <ybd4731@gmail.com>
2024-06-24 21:24:36 +0900

    feat: orElse() -> orElseThroe(() -> new Exception) 형태로 변경

--- src/main/java/com/nhnacademy/orderpaymentrefund/exception/PaymentNotFoundException.java
+++ src/main/java/com/nhnacademy/orderpaymentrefund/exception/PaymentNotFoundException.java
@@ -3,10 +3,6 @@ package com.nhnacademy.orderpaymentrefund.exception;
 import com.nhnacademy.orderpaymentrefund.exception.type.NotFoundExceptionType;
 
 public class PaymentNotFoundException extends NotFoundExceptionType {
-    public PaymentNotFoundException() {
-        super("결제 정보를 찾을 수 없습니다.");
-    }
-    public PaymentNotFoundException(String message) {
-        super(message);
-    }
+    public PaymentNotFoundException() { super("결제 정보를 찾을 수 없습니다."); }
+    public PaymentNotFoundException(String message) { super(message); }
 }
