f90478d8b46273c1d6a4e898ae4dd94fffee6d95
Virtus_Chae <ybd4731@gmail.com>
2024-06-24 15:59:30 +0900

    Dto Record -> @Data class 변경

--- src/main/java/com/nhnacademy/orderpaymentrefund/dto/payment/request/PaymentRequestDto.java
+++ src/main/java/com/nhnacademy/orderpaymentrefund/dto/payment/request/PaymentRequestDto.java
@@ -1,9 +1,16 @@
 package com.nhnacademy.orderpaymentrefund.dto.payment.request;
 
-public record PaymentRequestDto(
-    Long orderId,
-    Long paymentMethodId,
-    Long couponId,
-    Long payAmount
-) {
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+
+@Data // Getter, Setter, equals & hashCode, toString, NoArgs, AllArgs ...
+public class PaymentRequestDto {
+    @NotNull
+    private Long orderId;                   // 주문 아이디
+
+    private Long paymentMethodId;           // 결제 수단의 아이디
+
+    private Long couponId;                  // 이번 결제에서 사용할 쿠폰의 아이디
+
+    private Long payAmount;                 // 최종 결제한 금액
 }
