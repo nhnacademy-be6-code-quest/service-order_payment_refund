39f39572b9a8753d3a6dc0f8359c507c55432c32
Virtus_Chae <ybd4731@gmail.com>
2024-06-24 16:00:04 +0900

    chore: dto 변경에 따른 getter 변경

--- src/main/java/com/nhnacademy/orderpaymentrefund/service/payment/impl/PaymentServiceImpl.java
+++ src/main/java/com/nhnacademy/orderpaymentrefund/service/payment/impl/PaymentServiceImpl.java
@@ -25,15 +25,15 @@ public class PaymentServiceImpl implements PaymentService {
     // Order Enum Type -> String, 배송 상태 -> tinyInt
     @Override
     public void savePayment(PaymentRequestDto paymentRequestDto) {
-        Order order = orderRepository.findById(paymentRequestDto.orderId()).orElse(null);
+        Order order = orderRepository.findById(paymentRequestDto.getOrderId()).orElse(null);
 
         // 여기에서 이상이 있는 듯 -> Payment Method 가 없어서 문제가 생긴 것으로 보임
-        PaymentMethod paymentMethod = paymentMethodRepository.findById(paymentRequestDto.paymentMethodId()).orElse(null);
+        PaymentMethod paymentMethod = paymentMethodRepository.findById(paymentRequestDto.getPaymentMethodId()).orElse(null);
         Payment payment = Payment.builder()
             .order(order)
             .paymentMethod(paymentMethod)
-            .couponId(paymentRequestDto.couponId())
-            .payAmount(paymentRequestDto.payAmount())
+            .couponId(paymentRequestDto.getCouponId())
+            .payAmount(paymentRequestDto.getPayAmount())
             .payTime(LocalDateTime.now())
             .build();
 
