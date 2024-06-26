b7db9b47ed4360f8013e8933640314e04e9d92ee
bakhuiwon326 <powerup326@naver.com>
2024-06-26 14:10:32 +0900

    feat: add OrderService function

---
+++ src/main/java/com/nhnacademy/orderpaymentrefund/service/order/impl/OrderServiceImpl.java
@@ -0,0 +1,47 @@
+package com.nhnacademy.orderpaymentrefund.service.order.impl;
+
+import com.nhnacademy.orderpaymentrefund.domain.order.Order;
+import com.nhnacademy.orderpaymentrefund.domain.order.OrderStatus;
+import com.nhnacademy.orderpaymentrefund.dto.order.response.service.UserDetailGetResponseDto;
+import com.nhnacademy.orderpaymentrefund.exception.OrderNotFoundException;
+import com.nhnacademy.orderpaymentrefund.repository.order.OrderDetailRepository;
+import com.nhnacademy.orderpaymentrefund.repository.order.OrderRepository;
+import com.nhnacademy.orderpaymentrefund.service.order.OrderService;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@AllArgsConstructor
+public class OrderServiceImpl implements OrderService {
+
+    private OrderRepository orderRepository;
+    private OrderDetailRepository orderDetailRepository;
+
+    @Override
+    public long getTotalPrice(long orderId) {
+        Order order = orderRepository.findById(orderId).orElseThrow(() -> new OrderNotFoundException());
+        return order.getTotalPrice();
+    }
+
+    @Override
+    public List<UserDetailGetResponseDto> getUserDetails(long orderId) {
+        Order order = orderRepository.findById(orderId).orElseThrow(() -> new OrderNotFoundException());
+        return orderDetailRepository.findByOrder(order).stream().map(
+                orderDetail -> UserDetailGetResponseDto.builder()
+                        .productId(orderDetail.getProductId())
+                        .quantity(orderDetail.getQuantity())
+                        .priceProduct(orderDetail.getProductPrice())
+                        .build()
+        ).toList();
+    }
+
+    @Override
+    public void completePayment(long orderId) {
+        Order order = orderRepository.findById(orderId).orElseThrow(() -> new OrderNotFoundException());
+        order.updateOrder(OrderStatus.PAYED);
+        orderRepository.save(order);
+    }
+
+}
