07807a420fa845d91249420e614cdc1c4c80c38f
Virtus_Chae <ybd4731@gmail.com>
2024-06-30 13:16:27 +0900

    feat: 결제 수단 엔티티 수정

--- src/main/java/com/nhnacademy/orderpaymentrefund/domain/payment/PaymentMethod.java
+++ src/main/java/com/nhnacademy/orderpaymentrefund/domain/payment/PaymentMethod.java
@@ -15,12 +15,11 @@ public class PaymentMethod {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long paymentMethodId;
+    private long paymentMethodId;
 
     @NotNull
-    private String paymentMethodName;
+    private String paymentMethodName; // 토스 페이먼트가 제공하는 이름 - Enum 으로 해야 하는 이유가 있을까?
 
-    public PaymentMethod(String paymentMethodName) {
-        this.paymentMethodName = paymentMethodName;
-    }
+    @NotNull
+    private boolean paymentMethodIsUsed; // 우리 서비스에서 지원하는지 여부
 }
