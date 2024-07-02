2fae30ef0cc3ff3822e06df98239e80559550067
Virtus_Chae <ybd4731@gmail.com>
2024-07-02 10:23:18 +0900

    chore: 사용하지 않는 import 제거 (compile error 피하기 위해)

--- src/main/java/com/nhnacademy/orderpaymentrefund/service/payment/impl/PaymentServiceImpl.java
+++ src/main/java/com/nhnacademy/orderpaymentrefund/service/payment/impl/PaymentServiceImpl.java
@@ -1,24 +1,17 @@
 package com.nhnacademy.orderpaymentrefund.service.payment.impl;
 
-import com.nhnacademy.orderpaymentrefund.domain.order.Order;
 import com.nhnacademy.orderpaymentrefund.domain.payment.Payment;
-import com.nhnacademy.orderpaymentrefund.domain.payment.PaymentMethod;
 import com.nhnacademy.orderpaymentrefund.dto.payment.request.PaymentRequestDto;
 import com.nhnacademy.orderpaymentrefund.dto.payment.response.OrderPaymentResponseDto;
 import com.nhnacademy.orderpaymentrefund.dto.payment.response.PaymentResponseDto;
 import com.nhnacademy.orderpaymentrefund.exception.OrderNotFoundException;
-import com.nhnacademy.orderpaymentrefund.exception.PaymentNotFoundException;
 import com.nhnacademy.orderpaymentrefund.repository.order.OrderRepository;
-import com.nhnacademy.orderpaymentrefund.repository.payment.PaymentMethodRepository;
 import com.nhnacademy.orderpaymentrefund.repository.payment.PaymentRepository;
-import com.nhnacademy.orderpaymentrefund.service.order.OrderService;
 import com.nhnacademy.orderpaymentrefund.service.payment.PaymentService;
 import java.time.LocalDateTime;
-import java.util.List;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.PathVariable;
 
 @Slf4j
 @Service
@@ -33,7 +26,8 @@ public class PaymentServiceImpl implements PaymentService {
     public void savePayment(PaymentRequestDto paymentRequestDto, long orderId) {
 
         Payment payment = Payment.builder()
-            .order(orderRepository.findById(orderId).orElseThrow(() -> new OrderNotFoundException()))
+            .order(
+                orderRepository.findById(orderId).orElseThrow(() -> new OrderNotFoundException()))
             .payTime(LocalDateTime.now())
             .paymentMethodName("CARD")
             .tossPaymentKey(paymentRequestDto.getPaymentKey())
