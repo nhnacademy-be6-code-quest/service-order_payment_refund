82e4b4578577f42b9ef8b7ecab2ea98071722cd7
jjeonmin <jms959310@gmail.com>
2024-07-21 05:50:24 +0900

    feat: 환불 swagger추가

--- src/main/java/com/nhnacademy/orderpaymentrefund/controller/refund/RefundPolicyController.java
+++ src/main/java/com/nhnacademy/orderpaymentrefund/controller/refund/RefundPolicyController.java
@@ -1,31 +1,11 @@
 package com.nhnacademy.orderpaymentrefund.controller.refund;
 
 import com.nhnacademy.orderpaymentrefund.dto.refund.request.RefundPolicyRequestDto;
-import com.nhnacademy.orderpaymentrefund.dto.refund.request.RefundRequestDto;
-import com.nhnacademy.orderpaymentrefund.dto.refund.response.RefundPolicyResponseDto;
-import com.nhnacademy.orderpaymentrefund.service.refund.RefundPolicyService;
-import com.nhnacademy.orderpaymentrefund.service.refund.RefundService;
 import java.util.List;
-import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-@RequiredArgsConstructor
-public class RefundPolicyController {
-
-    private final RefundPolicyService refundPolicyService;
-    private final RefundService refundService;
+public interface RefundPolicyController {
 
     @GetMapping("/api/refund/refund-policy")
-    List<RefundPolicyRequestDto> findAllRefundPolicyRequestDtoList() {
-        return refundPolicyService.findAllRefundPolicyRequestDtoList();
-    }
-
-
-
-
-
+    List<RefundPolicyRequestDto> findAllRefundPolicyRequestDtoList();
 }
